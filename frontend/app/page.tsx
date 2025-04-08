db8e0a9a34e7638453389960802db3cacc8c21c0
Alexandre TAHI <alexandretahi7@gmail.com>
2025-04-08 21:29:48 +0200

    Home page improvement

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,14 +18,12 @@ export default function Page() {
     }, [false])
 
     return (
-        <div className="w-full h-full p-10 flex flex-col items-center gap-5">
-            <div className="w-full h-96 gap-5 relative flex flex-col items-center justify-center">
-                <CustomCarousel/>
-                <div className="sm:absolute w-full sm:w-4/5 inset-y-full flex items-center z-10">
-                    <HomeSearchWidget/>
-                </div>
+        <div className="w-full h-full p-6 flex flex-col items-center gap-5">
+            <CustomCarousel/>
+            <div className="w-full sm:w-4/5 sm:-mt-16 inset-y-full flex items-center z-10">
+                <HomeSearchWidget/>
             </div>
-            <div className="sm:mt-24 w-full flex flex-col gap-3 items-center justify-center">
+            <div className="w-full flex flex-col gap-3 items-center justify-center">
                 <div className="w-full flex flex-row items-center">
                     <h1 className={"text-3xl font-black"}>Evènements à venir</h1>
                 </div>
