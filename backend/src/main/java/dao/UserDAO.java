069b138b6991254265329351a2ecbc7069cefd09
ismaelOneDev <146163877+ismaelOneDev@users.noreply.github.com>
2025-04-18 01:31:37 +0200

    authentication management in the Backend

--- backend/src/main/java/dao/UserDAO.java
+++ backend/src/main/java/dao/UserDAO.java
@@ -1,10 +1,23 @@
 package dao;
 
 import entities.User;
+import rest.auth.PasswordUtil;
 
 public class UserDAO extends AbstractJpaDAO<Long, User> {
     public UserDAO() {
         super();
         setClazz(User.class);
+    }
+
+    /**
+     * Retrieves a User entity by its email address.
+     *
+     * @param email the email address of the user to retrieve
+     * @return the User entity with the specified email address
+     */
+    public User findByEmail(String email) {
+        return entityManager.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
+                .setParameter("email", email)
+                .getSingleResult();
     }
 }
